44974fb4dcc6ec16923c051ad6fb549b20c01b95
elyasaf Hershkovitz <Elyuk1988@gmail.com>
2020-04-01 17:51:07 +0300

    Updates

---
+++ precache-manifest.77be0710a66645a0cb1cbb867fa1e45a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83be05cff9f4e264c2f7315e118eeb55",
+    "url": "/monsters-app/index.html"
+  },
+  {
+    "revision": "74131f1200bc586dd332",
+    "url": "/monsters-app/static/css/main.e05d1e45.chunk.css"
+  },
+  {
+    "revision": "a8be786dd60095e58b85",
+    "url": "/monsters-app/static/js/2.f9ad0456.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-app/static/js/2.f9ad0456.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74131f1200bc586dd332",
+    "url": "/monsters-app/static/js/main.0019b5e1.chunk.js"
+  },
+  {
+    "revision": "01fb172248b3cb4bbe7c",
+    "url": "/monsters-app/static/js/runtime-main.d72d8566.js"
+  }
+]);
